237100fa218285bb42fa97d094b705afe3e61ba2
Alone Silva <alone.silva@reservafacil.tur.br>
2020-02-13 07:49:36 -0300

    Mudando arquivo de pastas e refactor na pipeline

--- frontend-pipeline.groovy
+++ frontend-pipeline.groovy
@@ -1,7 +1,8 @@
 podTemplate(
     label: 'questcode', 
     containers: [
-        containerTemplate(args: 'cat', name: 'docker', command: '/bin/sh -c', image: 'docker', ttyEnabled: true)
+        containerTemplate(args: 'cat', name: 'docker-container', command: '/bin/sh -c', image: 'docker', ttyEnabled: true),
+        containerTemplate(args: 'cat', name: 'helm-container', command: '/bin/sh -c', image: 'lachlanevenson/k8s-helm:v3.0.2', ttyEnabled: true)
     ],
     volumes: [
       hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')
@@ -9,21 +10,37 @@ podTemplate(
 )
 {
     node('questcode') {
-        def repos
+        def REPOS
+        def IMAGE_VERSION
+        def IMAGE_NAME = "frontend"
+        def ENVIRONMENT = "staging"
+        def GIT_REPOS_URL = "git@github.com:AlonePereira/frontend.git"
+        def CHARTMUSEUM_URL = "http://helm-chartmuseum:8080"
+
         stage('Checkout') {
-            repos = git branch: 'develop', credentialsId: 'github', url: 'git@github.com:AlonePereira/frontend.git'
+            REPOS = git branch: 'develop', credentialsId: 'github', url: GIT_REPOS_URL
+            IMAGE_VERSION = sh returnStdout: true, script: 'sh read-package-version.sh'
+            IMAGE_VERSION = IMAGE_VERSION.trim()
         }
         stage('Package') {
-            container('docker') {
+            container('docker-container') {
                 withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_HUB_PASSWORD', usernameVariable: 'DOCKER_HUB_USER')]) {
                     sh "docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}"
-                    sh "docker build -t alonesilva/frontend:0.1.4 . --build-arg NPM_ENV='staging'"
-                    sh 'docker push alonesilva/frontend:0.1.4'
+                    sh "docker build -t ${DOCKER_HUB_USER}/${IMAGE_NAME}:${IMAGE_VERSION} . --build-arg NPM_ENV='${ENVIRONMENT}'"
+                    sh "docker push ${DOCKER_HUB_USER}/${IMAGE_NAME}:${IMAGE_VERSION}"
                 }
             }
         }
         stage('Deploy') {
-            echo 'Iniciando Deploy com Helm'
+            container('helm-container') {
+                echo 'Iniciando Deploy com Helm'
+
+                sh """
+                    helm repo add questcode ${CHARTMUSEUM_URL}
+                    helm repo update
+                    helm upgrade staging-frontend questcode/frontend --namespace staging --set image.tag=${IMAGE_VERSION}
+                """
+            }
         }
     }
 }
